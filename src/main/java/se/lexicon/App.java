70a266a108016eeeb1dc6b82e983909ce5ffdb2a
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-04-05 12:34:22 +0200

    Declare and Run All the Methods from Namerepository

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,51 +2,42 @@ package se.lexicon;
 
 import java.util.*;
 public class App {
-    public static void main(String... k) {
-        NameRepository obj = new NameRepository();
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the name :- ");
-        int n = sc.nextInt();
-        String s[] = new String[n];
+    public static void main(String[] args) {
 
-
-        //Set the Array names :-
-
-        sc.nextLine();
-        for (int i = 0; i < n; i++) {
-            s[i] = sc.nextLine();
-        }
-        obj.setNames(s);
 
         // Get the size of Array:-
-        System.out.println("---SIZE---");
-        System.out.println(obj.getSize());
 
+        System.out.println("Initial Name size:- "+NameRepository.getsize());
+        String[] names={"Linda Anderson", "Sarah Hill","Jenny Anderson"};
+        NameRepository.setNames(names);
+        System.out.println("The Size of the Array name is :-"+NameRepository.getsize());
         //Display Array:-
 
-        System.out.println("---DISPLAY ALL---");
-        String a[] = obj.findAll();
-        obj.display(a);
+        System.out.println(names);
+        System.out.println(Arrays.toString(NameRepository.findall()));
+        //Clear Arraay:-
 
-        //Clear Arrays:_
-
-       System.out.println("---CLEAR ALL---");
-        obj.clear();
-        //obj.display(names);
-
-
-       //Search Name
-        System.out.println(obj.find("A B"));
-        System.out.println(obj.find("ZA ZA"));
-
-       //Add Name if not present
-        System.out.println(obj.add("A B"));
-        System.out.println(obj.add("ZA ZA"));
+        System.out.println(NameRepository.clear());
 
         //obj.display(names);
 
 
+        //Search Name
+        System.out.println(NameRepository.find("Sudipta Mishra"));
+
+        //Add Name if not present
+        System.out.println(NameRepository.add("Simon Elbrink"));
+        //obj.display(names);
+
+        System.out.println(Arrays.toString(NameRepository.findall()));
+        boolean newName = NameRepository.add("Sudipta Mishra");
+        if (newName){
+            System.out.println("Name is Added to Array");
+        } else {
+            System.out.println("Name is Duplicate!");
+        }
+        System.out.println(Arrays.toString(NameRepository.findByFirstName("Sudipta")));
 
     }
 }
